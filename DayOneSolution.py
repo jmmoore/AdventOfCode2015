3f5164dd5db0b205d7158209fb26b5aa73602349
Jacob Moore <jacob.m.moore@gmail.com>
2015-12-01 15:18:37 -0600

    Day One complete

---
+++ DayOneSolution.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python3
+
+import os
+
+def ParenthesisInterpreter(file):
+    floor = 0
+    resultList = list(open(file).read())
+    for index, char in enumerate(resultList, start=1):
+            if not char:
+                break
+            if char == "(":
+                floor += 1
+            elif char == ")":
+                floor -= 1
+
+# The commented lines complete part 2
+                #if floor == -1:
+                #    print("Hit the basement at position " + str(index))
+                #    break
+    return floor
+    
+#Execution
+
+pathToInput = os.path.abspath("/Users/JMMoore/santafloor")
+floorToSendSanta = ParenthesisInterpreter(pathToInput)
+print(floorToSendSanta)
